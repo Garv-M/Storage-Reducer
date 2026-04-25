a53088e1bc7c1e28d1324491e420236ecf77b0cc
Garv <garv.mittra@walmart.com>
2026-04-26 00:15:54 +0530

    removed unnecessary files

--- app/lock.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function LockScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>App Lock</Text>
-    </View>
-  );
-}
--- app/onboarding/lock-setup.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function LockSetupScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Lock Setup</Text>
-    </View>
-  );
-}
